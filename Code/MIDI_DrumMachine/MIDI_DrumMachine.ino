35da74fe49e71b85fde70d8c56f2601dbb88d15b
johnwmillr <john.w.millr@gmail.com>
2016-10-28 02:33:50 -0500

    Updated main drum machine code. Currently taking simultaneous inputs from 2 piezos and able to translate them into MIDI signals on the iPhone. Woo hoo

--- Code/MIDI_DrumMachine/MIDI_DrumMachine.ino
+++ Code/MIDI_DrumMachine/MIDI_DrumMachine.ino
@@ -1,137 +1,134 @@
 #define LED 13              // LED pin on Arduino Uno
-// Sensor details
-int n_sensors = 2;
-int sensorPin1 = A1;
-double sensorVal1 = 0.0;
-bool sensor1on = false;
-int threshold = 200;
+// Sensors
+const int n_sensors = 2;
+int    sensorPins[n_sensors] = {A1, A2};
+double sensorVals[n_sensors] = {0, 0};
+bool   sensorIsReady[n_sensors] = {false, false};
+int    threshold = 50; // Sensors must cross this level to be registered as a note
 
-// MIDI Details
-//int pitches[n_sensors] = {34, 35, 36, 37};
-//int pitches[n_sensors] = {38, 36};
-int vel = 120;
-int kickVel = 40;
-int note_delay = 10;
-int noteON  = 144; //144 = 10010000 in binary, note on command
-int noteOFF = 128; //128 = 10000000 in binary, note off command
-bool note_is_on = 0;
-bool turn_off_notes = 0;
-
-// Potentiometer
+// Potentiometer (selects sounds)
 int potPin = A0;
 double potVal = 0.0;
 
 // Buttons in place of sensors
-int button1 = 5;
-int button2 = 6;
-double pitch1 = 0;
-double pitch2 = 36;
+int button1 = 6;
+double pitch1 = 36;
 bool up1 = 1;
-bool up2 = 1;
-double t1 = 0;
-double t2 = 0;
 
+// MIDI
+bool noteIsOn[n_sensors] = {false, false};
+int  pitches[n_sensors] = {39, 39};
+int  noteVel = 0;
+int  newPitch = 0;
+double t_last_note[n_sensors];
+
+// Timing stuff
+int t_between_notes = 80; // Min. time until next trigger from sensor (ms)
+int note_duration = 250;  // Time until note is cut off (ms)
+
+// ----------------------------------------------------------------
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED, OUTPUT);
-  pinMode(button1, INPUT);
-  pinMode(button2, INPUT);  
+  pinMode(button1, INPUT); 
   Serial.begin(31250);
 }
 
-void loop() {
+// ----------------------------------------------------------------
+void loop() {  
   // Read value from pot, determine pitch value for the 1st sensor
   potVal = analogRead(potPin);  
-//  pitches[1] = floor((potVal/1023)*17+36);
-  pitch1 = floor((potVal/1023)*17+36);
+  newPitch = floor((potVal/1023)*17+36);  
 
-  // Check for button presses
-//  if (digitalRead(button1) == true and up1 == true){
-//    sendNoteOn(pitch1,vel);    
-//    note_is_on = 1;
-//    t1 = millis();
-//    up1 = false;
-//  }
-  if (digitalRead(button2) == true and up2 == true){
-    sendNoteOn(pitch2,kickVel);   
-    note_is_on = 1;
-    t2 = millis();
-    up2 = false;
-  }
+  // Check for sensor hits (threshold crossing)
+  // Also, turn any notes off if it's time to do so
+  for (int i = 0; i < n_sensors; i++){            
+    // Read from sensor and (maybe) trigger a note
+    sensorVals[i] = analogRead(sensorPins[i]);     
+    if (sensorVals[i] > threshold && sensorIsReady[i] == true) {      
+      if (noteIsOn[i] == true){
+        sendNoteOff(pitches[i]);
+      }
+      pitches[0] = newPitch;
+      noteVel = convertToVel(sensorVals[i], threshold);
+      sendNoteOn(pitches[i], noteVel); // Turn the MIDI note on
+      t_last_note[i] = millis();
+      noteIsOn[i] = true;
+      sensorIsReady[i] = false;
+    }    
 
-  // Check for sensor hits (over threshold)
-  sensorVal1 = analogRead(sensorPin1);
-//  Serial.println(sensorVal1);
-  if (sensorVal1 > threshold and sensor1on == false){
-    vel = floor((sensorVal1/1023)*127);
-    sendNoteOn(pitch1,vel);
-    note_is_on = 1;
-    t1 = millis();
-    sensor1on = true;
-  }
-  
-//  for (int i = 0; i < n_sensors; i++){
-//    double temp1 = analogRead(sensorPins[i]); 
-//    sensorVals[i] = temp1;
-//    Serial.println(temp1);
-//    if (sensorVals[i] > threshold and sensorsOn[i] == false) {
-//      vel = floor((potVal/1023)*17+36);
-//      sendNoteOn(pitches[i],vel);
-//      note_is_on = 1;
-//      t1 = millis();
-//      sensorsOn[i] = true;
-//    }
-//  }
-    
-  // Check for button release
-//  if (digitalRead(button1) == false){
-//    up1 = true;
-//  }
-  if (digitalRead(button2) == false){
-    up2 = true;
-  }
-
-  
-  // Check that the sensors are below threshold
-  double temp1 = analogRead(sensorPin1);
-  if (temp1 <= threshold){
-    sensor1on = false;
-  }
-  
-//  // Check that the sensors are below threshold
-//  for (int i = 0; i < n_sensors; i++){
-//    double temp2 = analogRead(sensorPins[i]);
-//    if (temp2 <= threshold){
-//      sensorsOn[i] = false;
-//    }
-//  }
-//  
-  // Turn notes off
-  if (note_is_on == true){
-    if (millis()-t1 > note_delay or millis()-t2 > note_delay){
-      for (int i = 0; i < n_sensors; i++){
-//        sendNoteOff(pitches[i]);
-        sendNoteOff(pitch1);
-        sendNoteOff(pitch2);
-      }           
-      note_is_on = 0;   
+    // Check if the current sensor is ready to be reactivated    
+    if (millis() - t_last_note[i] > t_between_notes){
+      sensorIsReady[i] = true;      
+      
+      // Turn off previous note if it's rang out long enough
+      if (noteIsOn[i] == true && (millis() - t_last_note[i] > note_duration)){
+        sendNoteOff(pitches[i]); // Turn off note
+        noteIsOn[i] = false;
+      }      
     }
-  }
-  delay(5);
+  } // End looping through checking each sensor  
+  
+  delay(1);
 } // End main loop()
 
 // -------------------------------------------------------------------
 // Define the internal functions for MIDI communication
 void sendNoteOn(int note, int velocity){  
-  int command  = 144; //144 = 10010000 in binary, note on command
+  int command  = 144; //144 = 10010000 in binary, note on command  
   Serial.write(command); // Send note on command
   Serial.write(note);
-  Serial.write(velocity); // Velocity value of 0 (turn the note off)
+  Serial.write(velocity); // Velocity value of 0 (turn the note off)  
 }
 
 void sendNoteOff(int note){  
-  int command = 128; // Note off, 128 = 10000000 in binary, note off command
+  int command = 128; // Note off, 128 = 10000000 in binary, note off command  
   Serial.write(command); // Send note on command
   Serial.write(note);
-  Serial.write(0); // Velocity value of 0 (turn the note off)
+  Serial.write(0); // Velocity value of 0 (turn the note off)  
 }
+
+int convertToVel(double raw, int threshold){
+  double outputVel;
+  double min = 20.0;
+  double max = 127;  
+  outputVel = floor(raw/8.0+10);
+  // outputVel = floor(max*(raw - threshold)/(max-min));
+  return outputVel;
+}
+
+void flashLED(){
+  for (int n=0;n<10;n++){
+    digitalWrite(LED, HIGH);
+    delay(50);
+    digitalWrite(LED, LOW);
+    delay(50);
+  }
+  delay(500);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
