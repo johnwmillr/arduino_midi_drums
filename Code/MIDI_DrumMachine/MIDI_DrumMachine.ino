08f63fd4cb53509ffa38b9ed7d9439cc619ecbfb
johnwmillr <john.w.millr@gmail.com>
2016-10-28 23:53:56 -0500

    Final code before the costume's debut! 4 sensors and 1 potentiometer for changing one sensor's notes.

--- Code/MIDI_DrumMachine/MIDI_DrumMachine.ino
+++ Code/MIDI_DrumMachine/MIDI_DrumMachine.ino
@@ -4,7 +4,6 @@ const int n_sensors = 4;
 int    sensorPins[n_sensors] = {A0, A1, A2, A3};
 double sensorVals[n_sensors] = {0, 0, 0, 0};
 bool   sensorIsReady[n_sensors] = {false, false, false, false};
-int    threshold = 50; // Sensors must cross this level to be registered as a note
 
 // Potentiometer (selects sounds)
 int potPin = A4;
@@ -13,6 +12,7 @@ double potVal = 0.0;
 // MIDI
 bool noteIsOn[n_sensors] = {false, false, false, false};
 int  pitches[n_sensors] = {36, 37, 38, 39};
+// int  pitches[n_sensors] = {36, 43, 38, 39};
 int  noteVel = 0;
 int  newPitch = 0;
 double t_last_note[n_sensors];
@@ -21,11 +21,14 @@ double t_last_note[n_sensors];
 int t_between_notes = 100; // Min. time until next trigger from sensor (ms)
 int note_duration = 500;  // Time until note is cut off (ms)
 
+int    threshold = 20; // Sensors must cross this level to be registered as a note
+
 // ----------------------------------------------------------------
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED, OUTPUT);  
   Serial.begin(31250);
+  // Serial.begin(9600);
 }
 
 // ----------------------------------------------------------------
@@ -36,15 +39,16 @@ void loop() {
 
   // Check for sensor hits (threshold crossing)
   // Also, turn any notes off if it's time to do so
-  for (int i = 0; i < n_sensors; i++){            
+  for (int i = 0; i < n_sensors; i++){                
     // Read from sensor and (maybe) trigger a note
     sensorVals[i] = analogRead(sensorPins[i]);     
-    if (sensorVals[i] > threshold && sensorIsReady[i] == true) {      
+    if (sensorVals[i] > threshold && sensorIsReady[i] == true) {            
       if (noteIsOn[i] == true){
         sendNoteOff(pitches[i]);
       }
-      pitches[0] = newPitch;
-      noteVel = convertToVel(sensorVals[i], threshold);
+      pitches[3] = newPitch;
+      noteVel = convertToVel(sensorVals[i], threshold);      
+      // noteVel = 127;
       sendNoteOn(pitches[i], noteVel); // Turn the MIDI note on
       t_last_note[i] = millis();
       noteIsOn[i] = true;
@@ -69,7 +73,7 @@ void loop() {
 // -------------------------------------------------------------------
 // Define the internal functions for MIDI communication
 void sendNoteOn(int note, int velocity){  
-  int command  = 144; //144 = 10010000 in binary, note on command  
+  int command  = 144; //144 = 10010000 in binary, note on command    
   Serial.write(command); // Send note on command
   Serial.write(note);
   Serial.write(velocity); // Velocity value of 0 (turn the note off)  
@@ -84,10 +88,14 @@ void sendNoteOff(int note){
 
 int convertToVel(double raw, int threshold){
   double outputVel;
-  double min = 20.0;
+  double min = 60.0;
   double max = 127;  
-  outputVel = floor(raw/8.0+10);
-  // outputVel = floor(max*(raw - threshold)/(max-min));
+  // outputVel = floor(raw/8.0+10);
+  outputVel = floor(max*(raw - threshold)/(max-min));
+  if (outputVel >= 115){
+    outputVel = max;
+  }
+
   return outputVel;
 }
 
